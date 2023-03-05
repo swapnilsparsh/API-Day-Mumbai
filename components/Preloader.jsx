b57544aa001e780e01c3a495ebfcbd68b43c6dfd
Pranit <pranit@webxstudio.in>
2023-03-05 23:56:37 +0530

    optimized preloading screen

--- components/Preloader.jsx
+++ components/Preloader.jsx
@@ -5,8 +5,8 @@ const Preloader = () => {
     <div className="bg-gray-700 w-screen h-screen flex justify-center items-center">
       <div className="flex p-1">
         <div className="text-right flex flex-col justify-center">
-          <h2 className="font-bold text-2xl">API SUMMIT 2023</h2>
-          <h2 className="font-semibold text-2xl italic">MUMBAI</h2>
+          <h2 className="font-bold text-xl md:text-2xl">API SUMMIT 2023</h2>
+          <h2 className="font-semibold text-xl md:text-2xl italic">MUMBAI</h2>
           <p>4 April 2023</p>
         </div>
         <div className="h-auto my-10 mx-5 w-1 bg-white"></div>
@@ -27,9 +27,9 @@ export default Preloader;
 
 const ChangingText = ({ text }) => {
   return (
-    <div className="block text-rotate h-full text-2xl px-2 ">
+    <div className="block text-rotate h-full max-md:text-xl text-2xl px-2 ">
       <div className="flex items-center h-full font-bold text-white ">
-        <p className="p-3 bg-primary italic uppercase"> {text}</p>
+        <p className="p-3 bg-primary italic uppercase -skew-x-12"> {text}</p>
       </div>
     </div>
   );
