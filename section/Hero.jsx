948292836df67dd30a53bbfa8e52f899be5c90d4
Pranit <pranit@webxstudio.in>
2023-04-10 00:05:08 +0530

    psp

--- section/Hero.jsx
+++ section/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             Lets Summit
           </p>
           <h1 className="gradient-text md:text-8xl text-6xl ">API DAY 2023</h1>
-          <p className="md:text-3xl text-lg py-10">April 30th, Mumbai, India</p>
+          <p className="md:text-3xl text-lg py-10">May 6th, Mumbai, India</p>
           <button className="px-8 py-4 bg-gray-600  transition-colors duration-300 hover:bg-primary text-white w-fit rounded-md">
             Get Tickets
           </button>
